357afc6eff082404b6b34c550b9c7bcd8c596391
FingerLinger43 <149538787+TheLegendFinger@users.noreply.github.com>
2025-02-15 05:59:23 -0500

    Update load.js

--- public/js/load.js
+++ public/js/load.js
@@ -2,7 +2,7 @@ const params = new URLSearchParams(window.location.search)
 if (params.get("game")) {
     games.forEach(async game => {
         if (game.id != params.get("game")) return
-        document.title = `${game.title} | Art Class`
+        document.title = `${game.title} | Cross Scoology`
         document.querySelector("#gameImage").src = game.image
         document.querySelector("#gameTitle").innerHTML = game.title
         if (game.description) document.querySelector("#gameDescription").innerHTML = game.description
